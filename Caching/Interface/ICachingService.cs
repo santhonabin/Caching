5c79a1409790b77f46e2e5dbc5145e3dd2c783b2
Priya <santhosepriya@gmail.com>
2020-07-09 14:54:41 +0530

    MemoryCache for Web API resonse using Action filter

---
+++ Caching/Interface/ICachingService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Caching.Interface
+{
+    public interface ICachingService<T> where T : class
+    {
+        /// <summary>
+        /// Get the cache value
+        /// </summary>
+        /// <param name="cacheId"></param>
+        /// <returns></returns>
+        IEnumerable<T> GetCache(object cacheId);
+
+        /// <summary>
+        /// Set the cache value
+        /// </summary>
+        /// <param name="cacheId"></param>
+        /// <param name="cacheValue"></param>
+        void SetCache(object cacheId, IEnumerable<T> cacheValue);
+
+        /// <summary>
+        /// Set the cache value with two types of Expiration : 
+        /// 1. Absolute expiration using TimeSpan ie., After certain period, the cache will expire
+        /// 2. Sliding expiration ie., evict if not accessed for particular number of days
+        /// </summary>
+        /// <param name="cacheId"></param>
+        /// <param name="cacheValue"></param>
+        void SetCacheWithExpiration(object cacheId, IEnumerable<T> cacheValue);
+
+        /// <summary>
+        /// Set the cache value with Sliding expiration ie., evict if not accessed for particular number of days
+        /// </summary>
+        /// <param name="cacheId"></param>
+        /// <param name="cacheValue"></param>
+        void SetCacheWithSlidingExpiration(object cacheId, IEnumerable<T> cacheValue);
+
+        /// <summary>
+        /// Clear Cache based on the Key
+        /// </summary>
+        /// <param name="key"></param>
+        void Remove(object key);
+    }
+}
---
+++ CachingApp/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using Caching.Interface;
+using CachingApp.Model;
+using Caching.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Generic;
+
+namespace CachingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IMemoryCache _cache;
+        private readonly ICachingService<Response> _cachingService;
+
+        public ValuesController(IMemoryCache cache, ICachingService<Response> cachingService)
+        {
+            _cache = cache;
+            _cachingService = cachingService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        [ServiceFilter(typeof(CachingAttribute))]
+        public ActionResult<IEnumerable<Response>> Get()
+        {
+            var items = new List<Response>();
+            items.Add(new Response { Id = 1, Name = "HS" });
+            items.Add(new Response { Id = 2, Name = "MP" });
+            return Ok(items);
+        }
+
+       
+        [HttpGet("{id}")]
+        [ServiceFilter(typeof(CachingAttribute))]
+        public ActionResult<IEnumerable<Response>> Get(int id)
+        {
+            var items = new List<Response>();
+            items.Add(new Response { Id = 2, Name = "MP" });
+            return Ok(items);
+        }
+
+        // POST api/values
+        [HttpPost]
+        [ServiceFilter(typeof(CachingAttribute))]
+        public void Post([FromBody] string value)
+        {
+           
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        [ServiceFilter(typeof(CachingAttribute))]
+        public void Put(int id, [FromBody] string value)
+        {
+            
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
